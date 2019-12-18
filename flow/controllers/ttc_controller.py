d79b079bba472acc039914ed225a78105c2f6576
africamonkey <africamonkey@github.com>
2019-12-18 18:27:10 +0800

    move controller & utils from diploma_paer

--- flow/controllers/ttc_controller.py
+++ flow/controllers/ttc_controller.py
@@ -1,1 +1,97 @@
-/Users/africamonkey/Documents/sysuclass/session7/diploma_paper/code/flow/ttc_controller.py
+import numpy as np
+from flow.utils import ttc_utils
+from flow.controllers.base_controller import BaseController
+
+
+MAX_DECEL = 4.5
+TTC_THRESHOLD = 4
+CONSECUTIVE_GREATER_THRESHOLD = 2
+DEBUG = False
+
+
+class TTCController(BaseController):
+    def __init__(self,
+                 veh_id,
+                 v0=30,
+                 T=1,
+                 a=1,
+                 b=1.5,
+                 delta=4,
+                 s0=2,
+                 time_delay=0.0,
+                 dt=0.1,
+                 noise=0,
+                 fail_safe=None,
+                 car_following_params=None):
+        """Instantiate an IDM controller."""
+        BaseController.__init__(
+            self,
+            veh_id,
+            car_following_params,
+            delay=time_delay,
+            fail_safe=fail_safe,
+            noise=noise)
+        self.v0 = v0
+        self.T = T
+        self.a = a
+        self.b = b
+        self.delta = delta
+        self.s0 = s0
+        self.dt = dt
+        self.go_action = False
+        self.greater_threshold = 0
+
+    def get_accel(self, env):
+        v = env.k.vehicle.get_speed(self.veh_id)
+        h = env.k.vehicle.get_headway(self.veh_id)
+        pos = env.k.vehicle.get_position(self.veh_id)
+        ori = env.k.vehicle.get_orientation(self.veh_id)
+        length = env.k.vehicle.get_length(self.veh_id)
+        if pos <= 25:
+            h = 25.1 - pos
+        else:
+            min_ttc = 1000
+            min_ori_oth = None
+            min_v_oth = None
+            for oth_id in env.k.vehicle.get_ids():
+                ori_oth = env.k.vehicle.get_orientation(oth_id)
+                v_oth = env.k.vehicle.get_speed(oth_id)
+                if oth_id != self.veh_id:
+                    ttc = ttc_utils.car_ttc(ori, ori_oth, v, v_oth)
+                    if min_ttc > ttc:
+                        min_ttc = ttc
+                        min_ori_oth = ori_oth
+                        min_v_oth = v_oth
+
+            if DEBUG:
+                print("min_ttc {0}, v {1}, ori {2}, v_oth {3}, ori_oth {4}"
+                      .format(min_ttc,
+                              v,
+                              ori,
+                              min_v_oth,
+                              min_ori_oth))
+
+            if min_ttc > TTC_THRESHOLD:
+                self.greater_threshold += 1
+                if self.greater_threshold >= CONSECUTIVE_GREATER_THRESHOLD:
+                    self.go_action = True
+            else:
+                self.greater_threshold = 0
+                # self.go_action = False
+
+            if not self.go_action:
+                return - MAX_DECEL
+        # in order to deal with ZeroDivisionError
+        if abs(h) < 1e-3:
+            h = 1e-3
+
+        lead_id = env.k.vehicle.get_leader(self.veh_id)
+        if lead_id is None or lead_id == '':
+            s_star = 0
+        else:
+            v_lead = env.k.vehicle.get_speed(lead_id)
+            s_star = self.s0 + max(
+                0, v * self.T + v * (v - v_lead) /
+                   (2 * np.sqrt(self.a * self.b)))
+
+        return self.a * (1 - (v / self.v0)**self.delta - (s_star / h)**2)
---
+++ flow/utils/ttc_utils.py
@@ -0,0 +1,89 @@
+import numpy as np
+
+
+EPS = 1e-6
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Segment:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+
+def cross_product(base, a, b):
+    """cross product. Point base, Point a, Point b"""
+    x1, y1, x2, y2 = a.x - base.x, a.y - base.y, b.x - base.x, b.y - base.y
+    return x1 * y2 - x2 * y1
+
+
+def sgn(x):
+    if x > EPS:
+        return 1
+    if x < -EPS:
+        return -1
+    return 0
+
+
+def cmp(x, y):
+    return sgn(x - y)
+
+
+def is_segment_intersect(l1, l2):
+    s1, e1, s2, e2 = l1.a, l1.b, l2.a, l2.b
+    if (cmp(min(s1.x, e1.x), max(s2.x, e2.x)) <= 0) and \
+       (cmp(min(s1.y, e1.y), max(s2.y, e2.y)) <= 0) and \
+       (cmp(min(s2.x, e2.x), max(s1.x, e1.x)) <= 0) and \
+       (cmp(min(s2.y, e2.y), max(s1.y, e1.y)) <= 0) and \
+       (sgn(cross_product(s1, s2, e2)) * sgn(cross_product(e1, s2, e2)) <= 0) and \
+       (sgn(cross_product(s2, s1, e1)) * sgn(cross_product(e2, s1, e1)) <= 0):
+        return True
+    return False
+
+
+def intersect_point(a, b):
+    s1 = cross_product(b.a, a.a, b.b)
+    s2 = cross_product(b.a, a.b, b.b)
+    return Point((a.b.x * s1 - a.a.x * s2) / (s1 - s2), (a.b.y * s1 - a.a.y * s2) / (s1 - s2))
+
+
+def det(a, b):
+    x1, y1, x2, y2 = a[0], a[1], b[0], b[1]
+    return x1 * y2 - x2 * y1
+
+
+def dis(a, b):
+    x1, y1, x2, y2 = a[0], a[1], b[0], b[1]
+    return np.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+
+
+def radius_convert(x):
+    return (450 - x) / 180 * np.pi
+
+
+def car_collide_point(ori_a, ori_b):
+    time_eval = 1000
+    angle_a = radius_convert(ori_a[2])
+    x1, y1 = ori_a[0], ori_a[1]
+    x2, y2 = x1 + np.cos(angle_a) * time_eval, y1 + np.sin(angle_a) * time_eval
+    sega = Segment(Point(x1, y1), Point(x2, y2))
+    angle_b = radius_convert(ori_b[2])
+    x1, y1 = ori_b[0], ori_b[1]
+    x2, y2 = x1 + np.cos(angle_b) * time_eval, y1 + np.sin(angle_b) * time_eval
+    segb = Segment(Point(x1, y1), Point(x2, y2))
+    if is_segment_intersect(sega, segb) is False:
+        return None
+    return intersect_point(sega, segb)
+
+
+def car_ttc(ori_a, ori_b, v_a, v_b):
+    col_point = car_collide_point(ori_a, ori_b)
+    if col_point is None or abs(v_b) < 1e-3:
+        return 1000
+    col_ori = [col_point.x, col_point.y]
+    return dis(ori_b, col_ori) / abs(v_b)
