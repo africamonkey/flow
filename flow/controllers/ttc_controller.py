aaf93b8eb02c2c327938f951c0fc6a1f572c8b6d
africamonkey <africamonkey@github.com>
2020-03-20 17:32:43 +0800

    ttc upd: ignore E2W

--- flow/controllers/ttc_controller.py
+++ flow/controllers/ttc_controller.py
@@ -48,19 +48,26 @@ class TTCController(BaseController):
         edg = env.k.vehicle.get_edge(self.veh_id)
         pos = env.k.vehicle.get_position(self.veh_id)
         ori = env.k.vehicle.get_orientation(self.veh_id)
+        rou = env.k.vehicle.get_route(self.veh_id)
 
         min_ttc = 1000
         min_ori_oth = None
         min_v_oth = None
         for oth_id in env.k.vehicle.get_ids():
+            if oth_id == self.veh_id:
+                continue
+            oth_route = env.k.vehicle.get_route(oth_id)
+            oth_e2w = (oth_route == ('E2M', 'M2WW', 'WW2W'))
+            self_e = (rou == ('SS2M', 'M2EE'))
+            if self_e and oth_e2w:
+                continue
             ori_oth = env.k.vehicle.get_orientation(oth_id)
             v_oth = env.k.vehicle.get_speed(oth_id)
-            if oth_id != self.veh_id:
-                ttc = ttc_utils.car_ttc(ori, ori_oth, v, v_oth)
-                if min_ttc > ttc:
-                    min_ttc = ttc
-                    min_ori_oth = ori_oth
-                    min_v_oth = v_oth
+            ttc = ttc_utils.car_ttc(ori, ori_oth, v, v_oth)
+            if min_ttc > ttc:
+                min_ttc = ttc
+                min_ori_oth = ori_oth
+                min_v_oth = v_oth
 
         if DEBUG:
             print("min_ttc {0}, v {1}, ori {2}, v_oth {3}, ori_oth {4}"
